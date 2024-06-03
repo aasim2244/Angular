5089f92f0ab2f3584a93c89b9676e927be0d019a
aasim <aasimpatel16@gmail.com>
2024-06-02 21:15:59 -0400

    child routes implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,29 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { SecondNavbarComponent } from './second-navbar/second-navbar.component';
 import { FirstNavbarComponent } from './first-navbar/first-navbar.component';
 import { TableComponent } from './table/table.component';
 import { FormComponent } from './form/form.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    { path: '', component: FirstNavbarComponent },
-    { path: 'second', component: SecondNavbarComponent },
-    { path: 'table', component: TableComponent, outlet:'navbar2' },
-    { path: 'form', component: FormComponent, outlet:'navbar2' }
-    
+    { path: '', component: FirstNavbarComponent, children: [
+        {
+          path: 'table', // child route path
+          component: TableComponent, // child route component that the router renders
+        },
+        {
+          path: 'form',
+          component: FormComponent, // another child route component that the router renders
+        }] 
+    },
+    { path: 'second', component: SecondNavbarComponent, children: [
+        {
+          path: 'table', // child route path
+          component: TableComponent, // child route component that the router renders
+        },
+        {
+          path: 'form',
+          component: FormComponent, // another child route component that the router renders
+        }]
+    }
 ];
