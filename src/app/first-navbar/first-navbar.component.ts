f8653e6dbebd93432edcc7587bd468885ffd4ebf
aasim <aasimpatel16@gmail.com>
2024-06-02 22:14:17 -0400

    search in navbar implemented

--- src/app/first-navbar/first-navbar.component.ts
+++ src/app/first-navbar/first-navbar.component.ts
@@ -1,23 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-first-navbar',
   standalone: true,
   imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, 
-    RouterModule],
+    RouterModule, MatFormField, MatLabel, MatInputModule],
   templateUrl: './first-navbar.component.html',
   styleUrl: './first-navbar.component.css'
 })
 export class FirstNavbarComponent {
-  @Output() onPlusClick = new EventEmitter<boolean>();
 
-  plusClick() {
-    this.onPlusClick.emit(true);
-    console.log('emit is: ', this.onPlusClick.subscribe());
+  value: string = '';
+  
+  setValue(e: any){
+    this.value = e.target.value; 
+    console.log(this.value);
   }
 }
