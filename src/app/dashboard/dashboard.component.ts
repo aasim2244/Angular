eb70e25019473818cdf194b54714dac485798995
aasim <aasimpatel16@gmail.com>
2024-06-06 23:49:51 -0400

    ngif implemented

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatGridListModule
-  ],
+  imports: [MatGridListModule, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  isVisibile: boolean = false;
 
 }
