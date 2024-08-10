11c99887d65892419199a253d8645fc7a453efe3
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-09 23:26:53 -0400

    added routing for all communication and ensured intra page ready for functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { TableComponent } from './table/table.component';
 import { FormComponent } from './form/form.component';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { IntraPageComponent } from './intra-page/intra-page.component';
+import { ParentChildComponent } from './parent-child/parent-child.component';
+import { AppServerComponent } from './app-server/app-server.component';
+import { AppS3Component } from './app-s3/app-s3.component';
+import { UnrelatedComponent } from './unrelated/unrelated.component';
+import { SiblingsComponent } from './siblings/siblings.component';
 
 export const routes: Routes = [
     { path: '', component: FirstNavbarComponent, children: [
@@ -17,8 +23,28 @@ export const routes: Routes = [
           component: FormComponent, // another child route component that the router renders
         },
         {
-          path: 'dashboard',
-          component: DashboardComponent, // another child route component that the router renders
+          path: 'intra',
+          component: IntraPageComponent
+        },
+        {
+          path: 'parent-child',
+          component: ParentChildComponent
+        },
+        {
+          path: 'siblings',
+          component: SiblingsComponent
+        },
+        {
+          path: 'unrelated',
+          component: UnrelatedComponent
+        },
+        {
+          path: 'app-s3',
+          component: AppS3Component
+        },
+        {
+          path: 'app-server',
+          component: AppServerComponent 
         }] 
     },
     { path: 'second', component: SecondNavbarComponent, children: [
