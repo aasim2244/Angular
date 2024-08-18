08e01da7f84b44dc7ef969bb8ce6e0ea9c336570
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-18 19:03:18 -0400

    added reactive form front end

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatLabel } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormField, MatInput,
+    MatLabel, MatSelectModule, MatDatepickerModule,
+    MatCheckboxModule, MatRadioModule, MatSlideToggleModule,
+    MatSliderModule, MatButtonModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+  OnFormSubmit(){
+    console.log('form submitted with data:');
+  }
 
+  inputKeyUp(){
+    console.log('inputKeyUp called');
+  }
 }
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -45,5 +45,4 @@ export class TemplateFormComponent {
       slider: 100
     });
   }
-
 }
