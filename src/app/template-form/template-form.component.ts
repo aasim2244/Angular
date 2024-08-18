6e768ee35d8996bcdd2417319866c733ce48650a
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-18 18:24:15 -0400

    added get/set functionality for template form

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatLabel } from '@angular/material/form-field';
@@ -10,6 +10,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSliderModule} from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-template-form',
   standalone: true,
@@ -17,7 +18,7 @@ import {MatSliderModule} from '@angular/material/slider';
   imports: [FormsModule, MatFormField, MatInput,
     MatLabel, MatSelectModule, MatDatepickerModule,
     MatCheckboxModule, MatRadioModule, MatSlideToggleModule,
-    MatSliderModule
+    MatSliderModule, MatButtonModule
   ],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
@@ -25,8 +26,24 @@ import {MatSliderModule} from '@angular/material/slider';
 
 export class TemplateFormComponent {
 
+  @ViewChild('formTemplateVar')
+  mainForm: NgForm;
+
+  OnFormSubmit(){
+    console.log('form submitted with data:');
+    
+    console.table(this.mainForm.value);
+  }
+
   inputKeyUp(param: any){
-    console.log(param);
+    console.log('inputKeyUp called');
+
+    this.mainForm.form.patchValue({
+      checkBoxInput: true,
+      radioButtonGroup: '1',
+      sliderToggle: true,
+      slider: 100
+    });
   }
 
 }
