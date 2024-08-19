7db832838372652817d9dcdb21e2064f30ecf12f
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-18 21:12:37 -0400

    added load and default values for reactive form controls

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatLabel } from '@angular/material/form-field';
@@ -10,17 +10,36 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSliderModule} from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [FormsModule, MatFormField, MatInput,
     MatLabel, MatSelectModule, MatDatepickerModule,
     MatCheckboxModule, MatRadioModule, MatSlideToggleModule,
-    MatSliderModule, MatButtonModule],
+    MatSliderModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+
+  reactiveForm: FormGroup;
+
+  constructor(){
+    this.reactiveForm = new FormGroup({
+
+      //sets the default values for the form
+      inputTextControl: new FormControl(null),
+      selectDropdownControl: new FormControl('opt1'),
+      datePickerInputControl: new FormControl(null),
+      checkboxControl: new FormControl(null),
+      radioGroupControl: new FormControl('1'),
+      toggleControl: new FormControl(null),
+      sliderControl: new FormControl(50)
+    });
+  }
+
   OnFormSubmit(){
     console.log('form submitted with data:');
   }
