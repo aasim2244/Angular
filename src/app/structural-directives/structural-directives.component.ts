cbe4dac0bf81392442fd49bae5615cc72f6fd10d
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-25 04:09:58 -0400

    added ngfor

--- src/app/structural-directives/structural-directives.component.ts
+++ src/app/structural-directives/structural-directives.component.ts
@@ -1,11 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatLabel } from '@angular/material/form-field';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor, NgForOf } from '@angular/common';
 @Component({
   selector: 'app-structural-directives',
   standalone: true,
-  imports: [MatSlideToggle, MatLabel, NgIf],
+  imports: [MatSlideToggle, MatLabel, NgIf, NgFor, NgForOf],
   templateUrl: './structural-directives.component.html',
   styleUrl: './structural-directives.component.css'
 })
@@ -16,6 +16,7 @@ export class StructuralDirectivesComponent implements
   slider: any;
 
   showContent: boolean = false;
+  list = [1,2,3,4];
 
   ngAfterViewInit(){
     this.slider._elementRef.nativeElement.onclick = () => this.handleSlider();
