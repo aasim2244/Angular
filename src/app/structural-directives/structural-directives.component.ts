a2370329bdd174bcbfdadf51658bd55707323d53
aasim2244 <136233984+aasim2244@users.noreply.github.com>
2024-08-26 10:33:40 -0400

    finished structural directives

--- src/app/structural-directives/structural-directives.component.ts
+++ src/app/structural-directives/structural-directives.component.ts
@@ -1,11 +1,15 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatLabel } from '@angular/material/form-field';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
-import { NgIf, NgFor, NgForOf } from '@angular/common';
+import { NgIf, NgFor, NgForOf, NgSwitch, NgSwitchCase, 
+  NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+
 @Component({
   selector: 'app-structural-directives',
   standalone: true,
-  imports: [MatSlideToggle, MatLabel, NgIf, NgFor, NgForOf],
+  imports: [MatSlideToggle, MatLabel, NgIf, NgFor, NgForOf,
+    NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet
+  ],
   templateUrl: './structural-directives.component.html',
   styleUrl: './structural-directives.component.css'
 })
@@ -18,11 +22,16 @@ export class StructuralDirectivesComponent implements
   showContent: boolean = false;
   list = [1,2,3,4];
 
+  switch: string = "one";
+  
   ngAfterViewInit(){
     this.slider._elementRef.nativeElement.onclick = () => this.handleSlider();
+    
   }
 
   handleSlider(){
     this.showContent = !this.showContent;
+    this.switch = "one";
+    console.log(this.showContent);
   }
 }
